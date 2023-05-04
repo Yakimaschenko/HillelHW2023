047f0a9d72691973a9107283c95aea8cf9679b18
Yakimaschenko Andrey <andrej.jakimascenko@privatbank.ua>
2023-05-04 19:23:54 +0300

    create AvgDynamicNumber

---
+++ src/com/hillel/HW2/AvgArithmeticNumber.java
@@ -0,0 +1,32 @@
+package src.com.hillel.HW2;
+
+import java.util.Scanner;
+
+public class AvgArithmeticNumber {
+
+
+    public static void main(String[] args) {
+        String value = "0.0";
+        Double avg = 0d;
+        Double sum = 0d;
+        Double number = 0d;
+        int colNumber = 0;
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите кол-во чисел: ");
+        colNumber = scan.nextInt();
+
+        for(int i=1; i<=colNumber; i++){
+            System.out.println("Введите число " + i + ": ");
+            number = scan.nextDouble();
+            sum += number;
+        }
+
+        avg = sum / colNumber;
+        System.out.println("Среднее арифметическое введенных чисел = " + avg);
+    }
+
+
+}
---
+++ src/com/hillel/HW2/AvgDynamicNumber.java
@@ -0,0 +1,33 @@
+package src.com.hillel.HW2;
+
+import java.util.Scanner;
+
+public class AvgDynamicNumber {
+
+
+    public static void main(String[] args) {
+        String values = "0.0";
+        Double avg = 0d;
+        Double sum = 0d;
+        Double number = 0d;
+        int colNumber = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        while (!values.equals("=")){
+            number = Double.parseDouble(values);
+            sum = sum + number;
+            System.out.println("Введите число или знак "+ "'='" + " для того, чтоб посчитать среднее арифметическое введенных чисел: ");
+            values = scan.nextLine();
+            colNumber++;
+        }
+        System.out.println(values);
+        System.out.println(sum);
+
+        avg = sum / --colNumber;
+        System.out.println("Cреднее арифметическое введенных чисел:" + avg);
+
+    }
+
+
+}

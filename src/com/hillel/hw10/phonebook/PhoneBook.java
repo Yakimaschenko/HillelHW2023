0529f2aba9517ec93bc4e62b4df9ae90495ed1d5
Yakimaschenko Andrey <andrej.jakimascenko@privatbank.ua>
2023-05-28 19:40:11 +0300

    create hw10

---
+++ src/com/hillel/hw10/phonebook/PhoneBook.java
@@ -0,0 +1,12 @@
+package src.com.hillel.hw10.phonebook;
+
+import java.util.ArrayList;
+
+public class PhoneBook {
+
+//    Private List<Subscriber>sub;
+//
+//    public PhoneBook(){
+//        this.sub = new ArrayList();
+//    }
+}
---
+++ src/com/hillel/hw10/phonebook/Subscriber.java
@@ -0,0 +1,4 @@
+package src.com.hillel.hw10.phonebook;
+
+public  class Subscriber {
+}

5f67b54eacd7647b9b8304a94c4c09ce480f83eb
Yakimaschenko Andrey <andrej.jakimascenko@privatbank.ua>
2023-06-04 20:06:54 +0300

    create hw10 FirstPart

---
+++ src/com/hillel/hw10/firstPart/FirstPart.java
@@ -0,0 +1,60 @@
+package src.com.hillel.hw10.firstPart;
+
+import java.util.*;
+
+public class FirstPart {
+//1. Створити метод countOccurance, що приймає на вхід рядковий список як параметр і довільний рядок.
+// Список заповнений довільними словами 10-20 штук, які можуть повторюватись у необмеженій кількості.
+// Порахувати скільки разів зустрічається переданий рядок як другий аргумент.
+
+
+    public int countOccurance(List<String> list, String value){
+        int count = 0;
+        for(String world : list){
+            if(world.equalsIgnoreCase(value)){
+                count++;
+            }
+        }
+        return count;
+    }
+    //2. Створити метод toList, що приймає на вхід цілісний масив довільної довжини.
+    // Конвертувати масив у список відповідного типу даних та повернути з методу.
+    //Наприклад:
+    //Було Array [1, 2, 3]
+    //Стало List [1, 2, 3]
+    public List<String> toList(String[] massiv){
+        List<String> list= new ArrayList<>();
+        for(String arr: massiv){
+            list.add(arr);
+        }
+        return list;
+    }
+//3. Створити метод findUnique, що приймає на вхід числовий список, що складається з довільних значень,
+// які можуть повторюватися в необмеженій кількості. Необхідно повернути новий числовий список, що містить тільки унікальні числа.
+    public Set<Integer> uniqueValues(List<Integer> values){
+    return new HashSet<>(values);
+}
+
+    //4. ** Створити метод calcOccurance, який приймає на вхід рядковий список як параметр.
+    // Список заповнений довільними словами 10-20 штук, які можуть повторюватись у необмеженій кількості.
+    // Обчислити скільки разів трапляється кожне слово. Результат вивести у консоль.
+    //Наприклад:
+    //bird: 2
+    //fox: 1
+    //cat: 1
+    public static void calcOccurance(List<String> list){
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : list) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            String world = entry.getKey();
+            int count = entry.getValue();
+            System.out.println(world + " = "+count);
+        }
+
+    }
+
+}
---
+++ src/com/hillel/hw10/firstPart/MainFirstPart.java
@@ -0,0 +1,39 @@
+package src.com.hillel.hw10.firstPart;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainFirstPart {
+    public static void main(String[] args) {
+        FirstPart firstPart = new FirstPart();
+
+        List<String> list= List.of("Книга","Книга","Мебель","Стул","Стол","Носок","Дверь", "Кросовок","Книга","Книга","Носок","Дверь", "Кросовок");
+        String value = "Книга";
+        System.out.println("-----...1...------");
+        int respond = firstPart.countOccurance(list, value);
+        System.out.println("В тексте указанное слово встречается - " + respond +" раз...");
+        System.out.println("------------");
+
+        System.out.println("-----...2...------");
+        String[] massiv = {"1","2","3","4","5","6","7","8","9"};
+        System.out.println("Було:  " + Arrays.deepToString(massiv));
+        System.out.println("Стало: " + firstPart.toList(massiv));
+        System.out.println("------------");
+
+        System.out.println("-----...3...------");
+        List<Integer> values = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 2, 2, 2, 4, 5, 6, 7);
+        System.out.println(firstPart.uniqueValues(values));
+        System.out.println("------------");
+
+        System.out.println("-----...4...------");
+        firstPart.calcOccurance(list);
+
+        System.out.println("------------");
+
+
+
+
+
+    }
+}
